43f95b7394a256d0cc0df421b0a7f36e7d787f2d
Crazypkr <Nicholas.lacasse430@gmail.com>
2026-03-19 00:31:23 -0400

    message for your changes. Lines starting

--- src/cards/light-card/light-card.ts
+++ src/cards/light-card/light-card.ts
@@ -201,7 +201,7 @@ export class LightCard
 
     this._activeControl = possibleControls[0];
   }
-+
+
 
   private _handleAction(ev: ActionHandlerEvent) {
     handleAction(this, this.hass!, this._config!, ev.detail.action!);
